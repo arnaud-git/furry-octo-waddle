990d815cf1c983b88672e6afe55b2c2f4c2aebdb
Franck Brazier <franck.brazier@gmail.com>
2016-06-09 17:10:59 -0400

    Instantiate BD_rw in the MainActivity + method 'print debug' for printing messages in log debug.

--- src/com/example/furry_octo_waddle/activities/MainActivity.java
+++ src/com/example/furry_octo_waddle/activities/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.furry_octo_waddle.activities;
 import com.example.furry_octo_waddle.R;
 import com.example.furry_octo_waddle.R.id;
 import com.example.furry_octo_waddle.R.layout;
+import com.example.furry_octo_waddle.sql_manager.Controleur_bd;
 
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
@@ -12,6 +14,10 @@ import android.os.Bundle;
 
 public class MainActivity extends ActionBarActivity {
 	
+	public static final String APPLICATION_TAG_NAME ="furry_octo_waddle"; 
+	public static final boolean DEBUG = true;
+	public static final int DEBUG_LEVEL = 1;
+	private static final boolean DEBUG_LEVEL_ACTIVATE = true;
 	Button buttonLearn;
 
 	@Override
@@ -20,15 +26,32 @@ public class MainActivity extends ActionBarActivity {
 		setContentView(R.layout.activity_main);
 		
 		buttonLearn = (Button)findViewById(R.id.buttonLearn);
+		final Controleur_bd cbd = new Controleur_bd(this);
 		buttonLearn.setOnClickListener(new View.OnClickListener() {
-
+	
+			
 			@Override
 			public void onClick(View arg0) {
 				// TODO Auto-generated method stub
 				Intent i = new Intent(MainActivity.this,LearnActivity.class);
                 startActivity(i);
+                //printDebug(1,"Lancement activite : "+i);
+                //cbd.test();
 			}
 		});
+	}
+	
+	public final static void printDebug(int level,String msg){
+		if(DEBUG){
+			if(DEBUG_LEVEL_ACTIVATE){
+				if(DEBUG_LEVEL == level){
+					Log.d(APPLICATION_TAG_NAME, msg);
+				}
+			}else {
+				Log.d(APPLICATION_TAG_NAME,"Level "+level+" : "+ msg);
+			}
+		}
+				
 	}
 
 }
