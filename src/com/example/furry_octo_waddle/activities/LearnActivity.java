e6c4650dd618bf424b59bdfb15816fadc6c81f7d
Franck Brazier <franck.brazier@gmail.com>
2016-06-26 20:19:45 -0400

    Fixed import in Learn Activity (merge)

--- src/com/example/furry_octo_waddle/activities/LearnActivity.java
+++ src/com/example/furry_octo_waddle/activities/LearnActivity.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.app.NavUtils;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
